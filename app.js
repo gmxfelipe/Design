709b8e1404de41d2292d8e4d85c1073d1c2f1ba8
Felipe Augusto Gomes de Oliveira <fagoliveira@fiecalunos.lan>
2020-02-17 20:33:15 -0300

    Add Gitignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyparser());
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(4200, () => {
     console.log('Server On')
